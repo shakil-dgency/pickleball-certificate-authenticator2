b5fbe75b45f12758013e2ff0d9de24fdcbf2000c
shakil <shakil.dgency@gmail.com>
2024-10-17 14:45:58 +0600

    logo click issue fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,21 @@ import Image from "next/image";
 import React from "react";
 import logo from "../../public/logo.svg";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function Navbar() {
+	const param = usePathname();
 	const handleReload = () => {
-		window.location.reload();
+		if (param === "/") {
+			window.location.reload();
+		}
 	};
 
 	return (
 		<div className="text-red-400 text-xl py-3 bg-[#232323]">
 			<div className="g__container">
 				<Link href={"/"}>
-					<Image src={logo} height={45} width={200} className="cursor-pointer" onClick={handleReload}  alt="" />
+					<Image src={logo} height={45} width={200} className="cursor-pointer" onClick={handleReload} alt="" />
 				</Link>
 			</div>
 		</div>
