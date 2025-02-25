f8d04a5ce26c9893713613259e1593c530b63653
shakil <shakil.dgency@gmail.com>
2025-02-25 19:13:03 +0600

    test commit

--- src/components/GlobalContainer.jsx
+++ src/components/GlobalContainer.jsx
@@ -1,5 +1,5 @@
-
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Hero from "./Home/Hero/Hero";
 import PurchaseIncludes from "./Home/PurchaseIncludes";
 import AboutAndGallery from "./Home/AboutAndGallery";
@@ -7,19 +7,16 @@ import BodyContent from "./IsVerifiedOk/BodyContent";
 import HeroOk from "./IsVerifiedOk/HeroOk";
 import HeroFailed from "./IsVerificationFaild/HeroFailed";
 import FailedMessage from "./IsVerificationFaild/FailedMessage";
-// import { useGlobalState } from "@/context/GlobalStateContext";
+import { useGlobalState } from "@/context/GlobalStateContext";
 
 function GlobalContainer() {
-	// const { post, input } = useGlobalState();
+	const { post, input } = useGlobalState();
 
-	let post = null;
-
-	// useEffect(() => {
-	// 	setTimeout(() => {
-	// 		window.scrollTo(0, 0);
-	// 	}, 200);
-	// },[ ,post]);
-
+	useEffect(() => {
+		setTimeout(() => {
+			window.scrollTo(0, 0);
+		}, 200);
+	},[ ,post]);
 	// const handleClik = ()=>{
 	// 	window.scrollTo(0, 0);
 
@@ -30,17 +27,17 @@ function GlobalContainer() {
 			<div className="">
 				{post === null && (
 					<div>
-						{/* <Hero /> */}
-						{/* <PurchaseIncludes />
-						<AboutAndGallery /> */}
+						<Hero />
+						<PurchaseIncludes />
+						<AboutAndGallery />
 					</div>
 				)}
 
 				{/* --------verification ok ----------- */}
 				{post !== null && post !== undefined && (
 					<div>
-						{/* <HeroOk /> */}
-						{/* <BodyContent post={post} /> */}
+						<HeroOk />
+						<BodyContent post={post} />
 					</div>
 				)}
 
@@ -48,8 +45,8 @@ function GlobalContainer() {
 
 				{post === undefined && (
 					<div>
-						{/* <HeroFailed /> */}
-						{/* <FailedMessage input={input} /> */}
+						<HeroFailed />
+						<FailedMessage input={input} />
 					</div>
 				)}
 			</div>
