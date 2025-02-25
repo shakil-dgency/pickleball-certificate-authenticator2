8862f9fda3e771e2e0c173792a0b97b13c0828e3
shakil <shakil.dgency@gmail.com>
2025-02-25 19:04:28 +0600

    test deploy

--- src/components/GlobalContainer.jsx
+++ src/components/GlobalContainer.jsx
@@ -1,5 +1,5 @@
-"use client";
-import React, { useEffect } from "react";
+
+import React from "react";
 import Hero from "./Home/Hero/Hero";
 import PurchaseIncludes from "./Home/PurchaseIncludes";
 import AboutAndGallery from "./Home/AboutAndGallery";
